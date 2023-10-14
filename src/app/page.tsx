ffc48db34e422934839946b4e0b98b2ddfd5d8b1
Raffy Amoguis <raffy.amoguis1@gmail.com>
2023-10-14 11:21:51 +0800

    updated home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,31 @@
 import { Button } from "@/components/ui/button";
+import TypewriterTitle from "@/components/TypewriterTitle";
+import Link from "next/link";
+import { ArrowRight } from "lucide-react";
 
 export default function Home() {
-  return <Button>Hello</Button>;
+  return (
+    <div className="bg-gradient-to-r grainy min-h-screen from-rose-100 to-teal-100">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <h1 className="font-semibold text-7xl text-center">
+          Advance Note{" "}
+          <span className="text-green-600 font-bold">with AI Assistant</span>
+        </h1>
+        <div className="mt-4"></div>
+        <h2 className="font-semibold text-3xl text-center text-slate-700">
+          <TypewriterTitle />
+        </h2>
+        <div className="mt-8"></div>
+
+        <div className="flex justify-center">
+          <Link href="/dashboard">
+            <Button className="bg-green-600">
+              Get started{" "}
+              <ArrowRight className="ml-2 w-5 h-5" strokeWidth={3} />
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
