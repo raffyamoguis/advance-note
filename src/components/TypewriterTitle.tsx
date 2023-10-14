1f1d744f983ab73b289f64b90c5746e10de75bf1
Raffy Amoguis <raffy.amoguis1@gmail.com>
2023-10-14 11:12:41 +0800

    added typewriter

---
+++ src/components/TypewriterTitle.tsx
@@ -0,0 +1,20 @@
+"use client";
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+type Props = {};
+
+const TypewriterTitle = (props: Props) => {
+  return (
+    <Typewriter
+      options={{
+        loop: true,
+      }}
+      onInit={(typewriter) => {
+        typewriter.typeString("Supercharged Productivity.").start();
+      }}
+    />
+  );
+};
+
+export default TypewriterTitle;
